20e9b895083feb3ea3d74d7a38bcc98751fff80e
pnija <nijap@techversantinfotech.com>
2017-08-05 11:06:58 +0530

    changes in events/utils.py

--- events/utils.py
+++ events/utils.py
@@ -6,29 +6,29 @@ from django.core.mail import EmailMessage
 from django.template import Context
 from django.template.loader import render_to_string
 # import imgkit
-from django.core.files import File
-from django.contrib.sites.shortcuts import get_current_site
+# from django.core.files import File
+# from django.contrib.sites.shortcuts import get_current_site
 
-def get_current_url(request):
-	current_site = get_current_site(request)
-	domain = current_site.domain
-	protocol = 'https' if request.is_secure() else 'http'
-	return '%s://%s' % (protocol, domain)
+# def get_current_url(request):
+# 	current_site = get_current_site(request)
+# 	domain = current_site.domain
+# 	protocol = 'https' if request.is_secure() else 'http'
+# 	return '%s://%s' % (protocol, domain)
 
-def createimage(html,event_obj,request):
-	try:
-		imgkit.from_file(html, 'out.jpg')
-	except:
-		#pass imgkit exception
-		pass
-	file = open('out.jpg')
-	image_file = File(file)
-	event_obj.confirm_image = image_file
-	event_obj.save()
-	img_url = event_obj.confirm_image.url
-	url = get_current_url(request)
-	url = url + img_url
-	return url
+# def createimage(html,event_obj,request):
+# 	try:
+# 		# imgkit.from_file(html, 'out.jpg')
+# 	except:
+# 		#pass imgkit exception
+# 		pass
+# 	file = open('out.jpg')
+# 	image_file = File(file)
+# 	event_obj.confirm_image = image_file
+# 	event_obj.save()
+# 	img_url = event_obj.confirm_image.url
+# 	url = get_current_url(request)
+# 	url = url + img_url
+# 	return url
 
 def send_email(to_email, message, event_obj):
 	subject = 'QRT 85 Registration'
