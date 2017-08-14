d9ecef259fa792329766e35b223f95e2b9ddcfeb
pnija <nijap@techversantinfotech.com>
2017-08-14 19:38:21 +0530

    template mail changes

--- events/utils.py
+++ events/utils.py
@@ -13,7 +13,7 @@ def send_email(to_email, message, event_obj):
 	subject = 'QRT 85 Registration'
 
 	cxt = {'event_register': event_obj }
-	content = render_to_string('mail_template/coupon.html', cxt)	
+	content = render_to_string('coupon_mail.html', cxt)	
 	from_email = settings.DEFAULT_FROM_EMAIL
 	msg = EmailMultiAlternatives(subject, 'hi', from_email, to=[to_email])
 	msg.attach_alternative(content, "text/html")
