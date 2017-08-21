af17d8ca5348bef4214190d348713a865d6eda03
pnija <nijap@techversantinfotech.com>
2017-08-21 17:29:52 +0530

    added django-import-export

--- events/admin.py
+++ events/admin.py
@@ -4,15 +4,15 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 from events.models import *
+from import_export.admin import ImportExportModelAdmin
 
-
-class EventUsersAdmin(admin.ModelAdmin):
+class EventUsersAdmin(ImportExportModelAdmin):
 	list_display = ('first_name', 'last_name', 'mobile', 'email')
 
-class RegisteredUsersAdmin(admin.ModelAdmin):
+class RegisteredUsersAdmin(ImportExportModelAdmin):
 	list_display = ('event_user', 'table', 'event', 'amount_paid', 'event_status')
 
-class TableAdmin(admin.ModelAdmin):
+class TableAdmin(ImportExportModelAdmin):
 	list_display = ('table_name', 'table_order', 'event')
 
 # Register your models here.
