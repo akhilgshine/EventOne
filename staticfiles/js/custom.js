ea4b6504e88500c98a1e0f96dff8c42e5d2960fe
nijap <akhil.shine@techversantinc.com>
2018-03-21 13:57:12 +0530

    template changes  added

--- staticfiles/js/custom.js
+++ staticfiles/js/custom.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
 	$(window).on('load', function() {
 	    var labels = ['weeks', 'days', 'hours', 'minutes', 'seconds'],
 	      nextYear = '2018/08/06',
-	      template = _.template($('#main-example-template').html()),
+	      template =template($('#main-example-template').html()),
 	      currDate = '00:00:00:00:00',
 	      nextDate = '00:00:00:00:00',
 	      parser = /([0-9]{2})/gi,
