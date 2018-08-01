90411dffd0ef14def8a6b64cf05792c350bf414e
nijap <akhil.shine@techversantinc.com>
2018-08-01 17:36:46 +0530

    coupon script updated

--- events/management/commands/existing_users_food_coupon.py
+++ events/management/commands/existing_users_food_coupon.py
@@ -8,7 +8,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         print 'start'
-        registered_user = RegisteredUsers.objects.all().filter(created_date__lt='2018-08-01')
+        registered_user = RegisteredUsers.objects.all()
         for users in registered_user:
             if users.event_status == 'Couple' or users.event_status == 'Couple_Informal':
                 [create_user_coupon_set(users.id) for _ in range(2)]
